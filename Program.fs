0a9588434486b57b84e2f8dae9a737b4eb7c7ec1
Devi7uke <dvlk8003@gmail.com>
2023-04-09 12:43:07 -0600

    Solicitud
    Entrada de datos por teclado

--- Program.fs
+++ Program.fs
@@ -24,6 +24,30 @@ let estado3 = [
     [0; 0; 0; 0; 0; 0; 0; 0; 0]
 ]
 
+let rec verTabla list = 
+    list
+    |> List.iter (fun list ->
+        list
+        |> List.iter (fun i ->
+            printf "%A  " i
+        )
+        printfn ""
+    )
+let rec leer (i : int) (j : int) (list : int list)=
+    match j with
+    | 81 -> list
+    | _->
+        printf "Ingresa el valor (%A, %A) :" (i) (j%9)
+        let num = System.Int32.TryParse (Console.ReadLine())
+        match num with
+        | (true, x) -> leer (j / 9) (j + 1) (list @ [x])
+        | (false, x) -> 
+            printf "------------------Valor invalido-----------------------\n"
+            leer i j list
+
+let estados = leer 0 0 [] |> List.chunkBySize 9
+printfn "\n%A" estados
+(*
 let t0 = System.DateTime.UtcNow
 match capitulo3.busquedaArbol DFS.estrategia (Sudoku.problema estado3) with
     | Some n -> 
@@ -31,4 +55,4 @@ match capitulo3.busquedaArbol DFS.estrategia (Sudoku.problema estado3) with
         printfn "Solucion: %A" n.estado
     | None -> printfn "Solucion no encontrada :("
 let delta = System.DateTime.UtcNow - t0
-printfn "Tiempo transcurrido: %A " delta
+printfn "Tiempo transcurrido: %A " delta*)
